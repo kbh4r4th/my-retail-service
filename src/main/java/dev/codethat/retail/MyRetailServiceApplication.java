f1c7053680879607366ba5790771da41bf0be148
bh4r4th <bh4r4th@gmail.com>
2019-06-05 23:53:59 -0700

    spring init

---
+++ src/main/java/dev/codethat/retail/MyRetailServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.codethat.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/codethat/retail/MyRetailServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package dev.codethat.retail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRetailServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
